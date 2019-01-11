28d2e7ce3e27091b47383951f2ccc898300b2a6a
olenapyrig <35273404+olenapyrig@users.noreply.github.com>
2019-01-11 22:48:54 +0200

    Update Main.java

--- stngraphy/src/main/java/data/Main.java
+++ stngraphy/src/main/java/data/Main.java
@@ -1,14 +1,27 @@
 package data;
+import data.MyImage;
+import java.io.IOException;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
 public class Main {
-    public static void main(String[] args) {
 
-        Steganograph steganograph = new Steganograph();
-        String message = "Hello Worlds";
-        String path = "cat.jpg";
-        steganograph.encode(message, path);
-        String decodeMessage;
-        decodeMessage = steganograph.decode("hidden.bmp");
-        System.out.println(decodeMessage);
+    public static void main(String[] args) throws IOException {
+
+
+        String text = "Hello, OLena";
+        String path = "./stenography";
+        String path2 = "./stenography";
+        MyImage img = new MyImage(path);
+        BufferedImage original = img.getImage(path);
+        byte[] colors = img.get_byte_data(original);
+        byte[] message = text.getBytes();
+        byte[] len = img.convert_to_byte(message.length);
+        img.coding(colors, len, 0);
+        img.coding(colors, message, 32);
+
+        img.setImage(original, new File(path2));
+        String decoded = img.decoding(path2);
+        System.out.println(decoded);
     }
-}
+    }
